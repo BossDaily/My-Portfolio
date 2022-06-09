285ab2ccda33f5b642d0aeb0acd0eebbc8cd472b
BossDaily <gboys2008@gmail.com>
2022-06-09 12:11:46 -0500

    copy pasta

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,28 +6,28 @@ const Home: NextPage = () => {
   return (
     <div className="m-0 font-helvetica text-white bg-[#10002B]">
       <section>
-        <h1>Content</h1>
-        <p>fara</p>
+        <h1>I am BossDaily</h1>
+        <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
+      </section>
+      <section className="bg-[#C77DFF]">
+        <h1>I am BossDaily</h1>
+        <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
       </section>
       <section>
-        <h1>Content</h1>
-        <p>fara</p>
+        <h1>I am BossDaily</h1>
+        <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
       </section>
       <section>
-        <h1>Content</h1>
-        <p>fara</p>
+        <h1>I am BossDaily</h1>
+        <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
       </section>
       <section>
-        <h1>Content</h1>
-        <p>fara</p>
+        <h1>I am BossDaily</h1>
+        <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
       </section>
       <section>
-        <h1>Content</h1>
-        <p>fara</p>
-      </section>
-      <section>
-        <h1>Content</h1>
-        <p>fara</p>
+        <h1>I am BossDaily</h1>
+        <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
       </section>
     </div>
   )
