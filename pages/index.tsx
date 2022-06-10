da7d28511f3b590f80d8093fa15680fa963493d3
BossDaily <gboys2008@gmail.com>
2022-06-10 17:31:17 -0500

    full width

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
         <h1>I am BossDaily</h1>
         <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
       </section>
-      <div className="aspect-[960/300]"></div>
+      <div className="aspect-[960/300] w-full bg-no-repeat "></div>
       <section className="bg-[#C77DFF]">
         <h1>I am BossDaily</h1>
         <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
