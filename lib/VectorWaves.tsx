b7bb39e69c594d2ef31d1b2934e0a395af42cee7
BossDaily <gboys2008@gmail.com>
2022-06-11 13:20:00 -0500

    syntax error fix

--- lib/VectorWaves.tsx
+++ lib/VectorWaves.tsx
@@ -8,7 +8,7 @@ interface Props {
 const VectorWaves: NextPage<Props> = (props) => {
     const { url } = props
     return (
-        <div styles={{backgroundImage:`url(${url})`}} className="aspect-[960/300] w-full bg-no-repeat bg-center bg-cover"></div>
+        <div style={{backgroundImage:`url(${url})`}} className="aspect-[960/300] w-full bg-no-repeat bg-center bg-cover"></div>
     );
 }
 
