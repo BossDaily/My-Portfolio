5676ac22c9509a3c439a01f6e732b60275794b73
BossDaily <gboys2008@gmail.com>
2022-06-11 14:26:31 -0500

    test with normal div

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
         <h1>I am BossDaily</h1>
         <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
       </section>
-      <VectorWaves url="https://raw.githubusercontent.com/BossDaily/My-Portfolio/main/public/hero_layered_waves1.svg" />
+      <div style={{ backgroundImage:`url('hero_layered_waves1.svg')` }} className="aspect-[960/300] w-full bg-no-repeat bg-center bg-cover" />
       <section className="bg-[#C77DFF]">
         <h1>I am BossDaily</h1>
         <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
