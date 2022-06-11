cda88d364615edb933e10d06a009bcbd72c22fe9
BossDaily <gboys2008@gmail.com>
2022-06-10 19:52:04 -0500

    imported component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import VectorWaves from '../lib/VectorWaves'
 
 const Home: NextPage = () => {
   return (
