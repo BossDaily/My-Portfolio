583dcec9f40d06e406d07f7f3612fddee19d0cf7
BossDaily <gboys2008@gmail.com>
2022-06-13 14:44:52 -0500

    made 2 vector waves components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextComponentType, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import VectorWaves from '../components/VectorWaves'
+import VectorWaves from '../components/Waves/BottomVectorWaves'
 
 const Home: NextPage = () => {
   return (
