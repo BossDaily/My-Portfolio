7b220d6d6f36b39c37ba4c50c66ca2a9eef35876
BossDaily <gboys2008@gmail.com>
2022-06-13 14:49:35 -0500

    imported

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextComponentType, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import VectorWaves from '../components/Waves/BottomVectorWaves'
+import BottomVectorWaves from '../components/Waves/BottomVectorWaves'
+import TopVectorWaves from '../components/Waves/TopVectorWaves'
 
 const Home: NextPage = () => {
   return (
@@ -10,12 +11,12 @@ const Home: NextPage = () => {
         <h1>I am BossDaily</h1>
         <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
       </section>
-      <VectorWaves url={'hero_layered_waves1.svg'} />
+      <BottomVectorWaves url={'hero_layered_waves1.svg'} />
       <section className="bg-[#C77DFF]">
         <h1>I am BossDaily</h1>
         <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
       </section>
-      <VectorWaves url={'stats_bottom_wave2.svg'} />
+      <BottomVectorWaves url={'stats_bottom_wave2.svg'} />
       <section>
         <h1>I am BossDaily</h1>
         <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
