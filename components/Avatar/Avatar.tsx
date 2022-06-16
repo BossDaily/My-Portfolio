1c067b959f5f66a152f08dffe804fd6d17d41415
BossDaily <gboys2008@gmail.com>
2022-06-16 10:51:49 -0500

    added image component

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -4,7 +4,7 @@ import Img from "./Img";
 const Avatar = () => {
     return (
         <div className="relative">
-            
+            <Img url='https://cdn.discordapp.com/avatars/274973338676494347/2af179fb4264675156eb2081fea20a51.png?size=1024' />
         </div>
     );
 }
