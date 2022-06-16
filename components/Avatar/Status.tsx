d23cfd1d05d47d9f992021f6a7a1023daff7d610
BossDaily <gboys2008@gmail.com>
2022-06-16 10:42:16 -0500

    status component

---
+++ components/Avatar/Status.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { NextPage } from "next";
+
+const Status = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default Status;
