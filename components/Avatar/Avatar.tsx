8e895212eb2eed9fb9ab9ed693f09f31cfa87a7a
BossDaily <gboys2008@gmail.com>
2022-06-16 18:29:54 -0500

    overflow css

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -1,12 +1,9 @@
 import Image from "next/image";
 import Img from "./Img";
-import Status from "./Status";
-
 const Avatar = () => {
     return (
-        <div className="relative">
-            <Img url="https://cdn.discordapp.com/avatars/274973338676494347/2af179fb4264675156eb2081fea20a51.png" />
-            <Status />
+        <div className="relative overflow-visible">
+            <Img url="https://cdn.discordapp.com/avatars/274973338676494347/2af179fb4264675156eb2081fea20a51.png" borderColor="" />
         </div>
     );
 }
