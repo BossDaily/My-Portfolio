09ebf298f1d18b8fa5cdc7eef760298f36ca4609
BossDaily <gboys2008@gmail.com>
2022-06-16 10:38:19 -0500

    new component for the image

---
+++ components/Avatar/Img.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { NextPage } from "next";
+
+const Img = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default Img;
