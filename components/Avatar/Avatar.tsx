7cdc2342bc5e625976a6c6666de20d115a4e390f
BossDaily <gboys2008@gmail.com>
2022-06-17 11:38:49 -0500

    added hook

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -3,12 +3,19 @@ import Img from "./Img";
 import { useLanyard } from "react-use-lanyard/dist";
 import type { Activity } from "react-use-lanyard/dist";
 import { NextPage } from "next";
+
 const Avatar: NextPage = () => {
+    const { loading, status } = useLanyard({
+        userId: '274973338676494347',
+        socket: true
+    })
+    
     return (
         <div className="relative overflow-visible">
             <Img url="https://cdn.discordapp.com/avatars/274973338676494347/2af179fb4264675156eb2081fea20a51.png" borderColor="" />
         </div>
     );
+
 }
 
 export default Avatar;
