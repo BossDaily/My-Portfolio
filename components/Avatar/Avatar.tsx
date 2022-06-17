e91307f07de20858b454d4b90e723a508cddba5f
BossDaily <gboys2008@gmail.com>
2022-06-17 11:37:08 -0500

    next page

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 import Img from "./Img";
 import { useLanyard } from "react-use-lanyard/dist";
 import type { Activity } from "react-use-lanyard/dist";
-const Avatar = () => {
+import { NextPage } from "next";
+const Avatar: NextPage = () => {
     return (
         <div className="relative overflow-visible">
             <Img url="https://cdn.discordapp.com/avatars/274973338676494347/2af179fb4264675156eb2081fea20a51.png" borderColor="" />
