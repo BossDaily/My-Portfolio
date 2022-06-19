5ca7f13df167c2910e650270692d945e9b408588
BossDaily <gboys2008@gmail.com>
2022-06-18 22:43:09 -0500

    content prop

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -26,7 +26,7 @@ const Avatar: NextPage = () => {
     } else if (loading === false && status?.discord_status === "idle") {
         return (
             <div className="relative overflow-visible">
-                <Tooltip style="dark" placement="bottom" trigger="hover" animation="duration-500">
+                <Tooltip content={`Idle on Discord ${platform}`} style="dark" placement="bottom" trigger="hover" animation="duration-500">
                     <Img url={`https://cdn.discordapp.com/avatars/${status.discord_user.id}/${status.discord_user.avatar}.png`} borderColor="rgb(250, 168, 26)" />
                 </Tooltip>
             </div>
