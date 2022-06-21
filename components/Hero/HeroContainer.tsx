c5475ebe58009ea0a30c2881b09f1bc5baf922e7
BossDaily <gboys2008@gmail.com>
2022-06-21 11:23:27 -0500

    changed text size

--- components/Hero/HeroContainer.tsx
+++ components/Hero/HeroContainer.tsx
@@ -6,7 +6,7 @@ type Props = {
 }
 const HeroContainer: NextPage<Props> = ({ children }) => {
     return (
-        <div className="flex flex-col gap-8 sm:flex-row font-header items-center text-xl">
+        <div className="flex flex-col gap-8 sm:flex-row font-header items-center text-6xl">
             {children}
         </div>
     );
