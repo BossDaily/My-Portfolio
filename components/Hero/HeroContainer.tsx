f3a444b7833d21cef22941fe38f89785df05ec28
BossDaily <gboys2008@gmail.com>
2022-06-21 11:19:14 -0500

    added font and alighnment

--- components/Hero/HeroContainer.tsx
+++ components/Hero/HeroContainer.tsx
@@ -6,7 +6,7 @@ type Props = {
 }
 const HeroContainer: NextPage<Props> = ({ children }) => {
     return (
-        <div className="flex flex-col gap-8 sm:flex-row">
+        <div className="flex flex-col gap-8 sm:flex-row font-header content-center">
             {children}
         </div>
     );
