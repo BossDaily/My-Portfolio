5ef3b49c42ec20ef44c11e6cbd213e3ad5be8529
BossDaily <gboys2008@gmail.com>
2022-06-21 10:10:12 -0500

    children props

--- components/Hero/HeroContainer.tsx
+++ components/Hero/HeroContainer.tsx
@@ -1,7 +1,13 @@
-const HeroContainer = () => {
+import { NextPage } from "next";
+import { ReactNode } from "react";
+
+type Props = {
+    children: ReactNode
+}
+const HeroContainer: NextPage<Props> = ({ children }) => {
     return (
         <div>
-            Enter
+            {children}
         </div>
     );
 }
