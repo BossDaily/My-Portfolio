7c94e86e652dd5be8d3e7e817a41b305e0ea7ada
BossDaily <gboys2008@gmail.com>
2022-06-23 15:13:58 -0500

    fixed oppsie

--- components/Stats/StatsContainer.tsx
+++ components/Stats/StatsContainer.tsx
@@ -7,7 +7,7 @@ type Props = {
 const StatsContainer: NextPage<Props> = ({ children }) => {
     return (
         <div className="bg-Windsor max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
-            <ul class="grid grid-cols-2 gap-4 rounded-xl lg:grid-cols-4">
+            <ul className="grid grid-cols-2 gap-4 rounded-xl lg:grid-cols-4">
                 {children}
             </ul> 
         </div>
