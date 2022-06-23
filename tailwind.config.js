91feca2988645badf253b2df7df4e2bbbcb1518a
BossDaily <gboys2008@gmail.com>
2022-06-23 11:01:14 -0500

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
       backgroundImage: {
         waves: 'https://raw.githubusercontent.com/BossDaily/My-Portfolio/main/public/stats_bottom_wave1.svg'
       },
+      colors: {
+        'BlackRussian': '#10002B',
+        'Windsor': '#3C096C',
+        'Heliotrope': '#C77DFF',
+        'Medium-Purple': '#B469EF',
+        'Purple-Heart': '#7B2CBF'
+      }
     },
     fontFamily: {
       'header': ['Helvetica', 'Arial', 'sans-serif'],
