81a78b229f3a94521dad166d17cbd8905bd81650
BossDaily <gboys2008@gmail.com>
2022-06-23 17:10:01 -0500

    changed to import icon

--- components/Stats/StatsItem.tsx
+++ components/Stats/StatsItem.tsx
@@ -11,7 +11,7 @@ const StatsItem: NextPage<Props> = ({ icon, stat }) => {
     return (
         <div>
             <li className="p-8">
-                <Image />
+                <Image src={icon}  />
                 <p className="mt-1 text-10xl font-medium">{stat}</p>
             </li>
         </div>
