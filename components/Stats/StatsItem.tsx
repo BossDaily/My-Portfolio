a6de9e4f4917ad7e3611d7a4ce3f1bf12f78a2a3
BossDaily <gboys2008@gmail.com>
2022-06-23 16:26:03 -0500

    icon

--- components/Stats/StatsItem.tsx
+++ components/Stats/StatsItem.tsx
@@ -1,17 +1,18 @@
+import { IconName } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { NextPage } from "next";
 import { ReactNode } from "react";
 
 type Props = {
-    icon: string
+    icon: IconName
     stat: string
 }
 const StatsItem: NextPage<Props> = ({ icon, stat }) => {
     return (
         <div>
             <li className="p-8">
-                <FontAwesomeIcon />
-                <p className="mt-1 text-10xl font-medium">Websites</p>
+                <FontAwesomeIcon icon={['fas', `${icon}`]} />
+                <p className="mt-1 text-10xl font-medium">{stat}</p>
             </li>
         </div>
     );
