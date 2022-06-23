477317a130960c9dca90b725761c2079f210e46a
BossDaily <gboys2008@gmail.com>
2022-06-23 17:07:28 -0500

    ez

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
