142cf7edddbde782b77b5fc287665de173a58c3e
BossDaily <gboys2008@gmail.com>
2022-06-23 15:19:05 -0500

    rounded corners

--- components/Stats/StatsContainer.tsx
+++ components/Stats/StatsContainer.tsx
@@ -6,7 +6,7 @@ type Props = {
 }
 const StatsContainer: NextPage<Props> = ({ children }) => {
     return (
-        <div className="bg-Windsor max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+        <div className="bg-Windsor max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8 rounded-xl">
             <ul className="grid grid-cols-2 gap-4 rounded-xl lg:grid-cols-4">
                 {children}
             </ul> 
