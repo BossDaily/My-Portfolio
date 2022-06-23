726ed1fee204ffb6d090df6ce0e29033e4132e1b
BossDaily <gboys2008@gmail.com>
2022-06-23 15:21:49 -0500

    initial file

---
+++ components/Stats/StatsItem.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+import { ReactNode } from "react";
+
+type Props = {
+    
+}
+const StatsItem = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default StatsItem;
