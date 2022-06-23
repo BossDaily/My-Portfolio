3bbb3e007c65072671e33acd783213957724c837
BossDaily <gboys2008@gmail.com>
2022-06-23 16:24:42 -0500

    font awesome

--- components/Stats/StatsItem.tsx
+++ components/Stats/StatsItem.tsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { NextPage } from "next";
 import { ReactNode } from "react";
 
@@ -8,7 +9,10 @@ type Props = {
 const StatsItem: NextPage<Props> = ({ icon, stat }) => {
     return (
         <div>
-            
+            <li className="p-8">
+                <FontAwesomeIcon />
+                <p className="mt-1 text-10xl font-medium">Websites</p>
+            </li>
         </div>
     );
 }
