77547fdbb595bab9417aa957ef31d21df93dd3f1
BossDaily <gboys2008@gmail.com>
2022-06-23 17:26:15 -0500

    idk what i put here

--- components/Stats/Stats.tsx
+++ components/Stats/Stats.tsx
@@ -6,6 +6,9 @@ const Stats = () => {
     return (
         <div>
             <StatsItem stat="faffdhsjfksdahjfks" icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'/>
+            <StatsItem stat="faffdhsjfksdahjfks" icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'/>
+            <StatsItem stat="faffdhsjfksdahjfks" icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'/>
+            
         </div>
     );
 }
