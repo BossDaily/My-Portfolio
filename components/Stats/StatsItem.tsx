0b4b03b3840cb868e7274276027765f32a64cc57
BossDaily <gboys2008@gmail.com>
2022-06-24 10:59:37 -0500

    changed to div

--- components/Stats/StatsItem.tsx
+++ components/Stats/StatsItem.tsx
@@ -10,10 +10,10 @@ type Props = {
 const StatsItem: NextPage<Props> = ({ icon, stat }) => {
     return (
         <div>
-            <li className="p-8">
+            <div className="p-8">
                 <Image src={icon} height={100} width={100} className="invert" />
                 <p className="mt-1 text-10xl font-medium">{stat}</p>
-            </li>
+            </div>
         </div>
     );
 }
