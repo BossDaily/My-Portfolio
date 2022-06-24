535a4683525225a62b58c7cc3d5e532d71344bbe
BossDaily <gboys2008@gmail.com>
2022-06-24 10:56:24 -0500

    changed to flex div

--- components/Stats/StatsContainer.tsx
+++ components/Stats/StatsContainer.tsx
@@ -7,9 +7,9 @@ type Props = {
 const StatsContainer: NextPage<Props> = ({ children }) => {
     return (
         <div className="bg-Windsor max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8 rounded-xl">
-            <ul className="grid grid-cols-2 gap-4 rounded-xl lg:grid-cols-4">
+            <div className="flex flex-row-2 gap-4 rounded-xl lg:flex-row-1">
                 {children}
-            </ul> 
+            </div> 
         </div>
     );
 }
