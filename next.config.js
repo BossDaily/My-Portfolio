9e15e610570c142b4d71d148f933d4beb3fe9a85
BossDaily <gboys2008@gmail.com>
2022-06-24 11:29:54 -0500

    idk

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['cdn.discordapp.com', 'cdn.jsdelivr.net']
+    domains: ['cdn.discordapp.com', 'cdn.jsdelivr.net'],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   typescript: {
     ignoreBuildErrors: true
