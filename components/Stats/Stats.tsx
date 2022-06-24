6c51fc8c293133ff0a6390add95d814a49cad173
BossDaily <gboys2008@gmail.com>
2022-06-24 11:35:30 -0500

    added font awesome

--- components/Stats/Stats.tsx
+++ components/Stats/Stats.tsx
@@ -8,8 +8,8 @@ const Stats = () => {
         <div>
             <StatsContainer>
                 <StatsItem stat="faffdhsjfksdahjfks" icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'/>
+                <StatsItem stat="faffdhsjfksdahjfks" icon='https://site-assets.fontawesome.com/releases/v6.1.1/svgs/solid/display-code.svg'/>
                 <StatsItem stat="faffdhsjfksdahjfks" icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg'/>
-                <StatsItem stat="faffdhsjfksdahjfks" icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'/>
             </StatsContainer>
         </div>
     );
