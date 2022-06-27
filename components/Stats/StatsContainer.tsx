1520f1ee6b8bea322a716d3649785ecf50dd78f0
BossDaily <gboys2008@gmail.com>
2022-06-27 12:03:20 -0500

    optimized width for ipads

--- components/Stats/StatsContainer.tsx
+++ components/Stats/StatsContainer.tsx
@@ -6,8 +6,8 @@ type Props = {
 }
 const StatsContainer: NextPage<Props> = ({ children }) => {
     return (
-        <div className="bg-Windsor max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8 rounded-xl flex flex-col sm:flex-row ">
-            <div className="flex flex-col gap-20 rounded-xl sm:flex-row">
+        <div className="bg-Windsor max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8 rounded-xl flex flex-col sm:flex-row  ">
+            <div className="flex flex-col gap-20 rounded-xl lg:flex-row">
                 {children}
             </div> 
         </div>
