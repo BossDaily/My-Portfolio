e733f706bb245579ecc5d0efb2731170aed19989
BossDaily <gboys2008@gmail.com>
2022-06-27 11:28:12 -0500

    better mobile responsiveness

--- components/Stats/StatsContainer.tsx
+++ components/Stats/StatsContainer.tsx
@@ -6,7 +6,7 @@ type Props = {
 }
 const StatsContainer: NextPage<Props> = ({ children }) => {
     return (
-        <div className="bg-Windsor max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8 rounded-xl">
+        <div className="bg-Windsor max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8 rounded-xl flex flex-col sm:flex-row ">
             <div className="flex flex-col gap-20 rounded-xl sm:flex-row">
                 {children}
             </div> 
