6d0734b8000d858c961a72f87195df0d57220399
BossDaily <gboys2008@gmail.com>
2022-06-27 11:28:01 -0500

    removed redundant divs

--- components/Stats/Stats.tsx
+++ components/Stats/Stats.tsx
@@ -5,13 +5,11 @@ import StatsContainer from "./StatsContainer";
 
 const Stats = () => {
     return (
-        <div>
-            <StatsContainer>
-                <StatsItem stat="faffdhsjfksdahjfks" icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'/>
-                <StatsItem stat="faffdhsjfksdahjfks" icon='https://site-assets.fontawesome.com/releases/v6.1.1/svgs/solid/display-code.svg'/>
-                <StatsItem stat="faffdhsjfksdahjfks" icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg'/>
-            </StatsContainer>
-        </div>
+        <StatsContainer>
+            <StatsItem stat="faffdhsjfksdahjfks" icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'/>
+            <StatsItem stat="faffdhsjfksdahjfks" icon='https://site-assets.fontawesome.com/releases/v6.1.1/svgs/solid/display-code.svg'/>
+            <StatsItem stat="faffdhsjfksdahjfks" icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg'/>
+        </StatsContainer>
     );
 }
 
