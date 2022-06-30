04f8c93930a7867eedd79f0a0c5c3b3bfd5bd27b
BossDaily <gboys2008@gmail.com>
2022-06-30 12:54:13 -0500

    proper use of headers

--- components/Stats/Stats.tsx
+++ components/Stats/Stats.tsx
@@ -6,25 +6,26 @@ import { GetServerSideProps } from 'next';
 
 export const getServerSideProps:GetServerSideProps = async (ctx) => {
 
-    const options = {
-        'content-type': 'application/json',
-        Authorization: `Basic ${process.env.WAKATIME}`
+    const options:RequestInit = {
+        method: 'POST',
+        headers: {'content-type': 'application/json', Authorization: `Basic ${process.env.WAKATIME}` }
     }
-    const req = await fetch('https://wakatime.com/api/v1/users/current/all_time_since_today', options)
+    const reqCodeTime = await fetch('https://wakatime.com/api/v1/users/current/all_time_since_today', options)
+    const totalCodeTime = await reqCodeTime.json()
 
     return {
         props:{
-            data:null
+            wakatimeTotal:totalCodeTime.data.text
         }
     }
 }
 
 
 
-const Stats = () => {
+const Stats = (wakatimeTotal: string) => {
     return (
         <StatsContainer>
-            <StatsItem stat="faffdhsjfksdahjfks" icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'/>
+            <StatsItem stat={`faffdhsjfksdahjfks ${wakatimeTotal}`} icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'/>
             <StatsItem stat="faffdhsjfksdahjfks" icon='https://site-assets.fontawesome.com/releases/v6.1.1/svgs/solid/display-code.svg'/>
             <StatsItem stat="faffdhsjfksdahjfks" icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg'/>
         </StatsContainer>
