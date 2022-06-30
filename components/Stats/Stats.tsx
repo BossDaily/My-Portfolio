d931bf4a6d6171f0b99107573097064d862c7bba
BossDaily <gboys2008@gmail.com>
2022-06-30 15:08:11 -0500

    type and more props

--- components/Stats/Stats.tsx
+++ components/Stats/Stats.tsx
@@ -1,28 +1,32 @@
 import StatsItem from "./StatsItem";
 import { IconName } from "@fortawesome/fontawesome-svg-core";
 import StatsContainer from "./StatsContainer";
-import { GetServerSideProps } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
 
+type Wakatime = {
+    TotalTime: string
+}
 
-export const getServerSideProps:GetServerSideProps = async (ctx) => {
+export const getServerSideProps:GetServerSideProps = async () => {
 
     const options:RequestInit = {
-        method: 'POST',
-        headers: {'content-type': 'application/json', Authorization: `Basic ${process.env.WAKATIME}` }
+        method: 'GET',
+        headers: {'content-type': 'application/json', Authorization: `Basic OGQ1YmI5MDctMmY1OC00ODRhLWIxZTMtMTU3OTg5YmJiZjVm` }
     }
-    const reqCodeTime = await fetch('https://wakatime.com/api/v1/users/current/all_time_since_today', options)
-    const totalCodeTime = await reqCodeTime.json()
-
+    const req = await fetch('https://wakatime.com/api/v1/users/current/all_time_since_today', options)
+    const time = await req.json()
+    console.log(time)
     return {
         props:{
-            wakatimeTotal:totalCodeTime.data.text
+            wakatimeTotal:time
         }
     }
 }
 
 
 
-const Stats = (wakatimeTotal: string) => {
+const Stats:NextPage<{ time }> = (props) => {
+    console.log(props)
     return (
         <StatsContainer>
             <StatsItem stat={`faffdhsjfksdahjfks ${wakatimeTotal}`} icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'/>
