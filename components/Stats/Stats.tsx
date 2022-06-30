08861bc5735510e82c0457e5089597013420d685
BossDaily <gboys2008@gmail.com>
2022-06-30 12:40:33 -0500

    options to query

--- components/Stats/Stats.tsx
+++ components/Stats/Stats.tsx
@@ -10,7 +10,7 @@ export const getServerSideProps:GetServerSideProps = async (ctx) => {
         'content-type': 'application/json',
         Authorization: `Basic ${process.env.WAKATIME}`
     }
-    const req = await fetch('https://wakatime.com/api/v1/users/current/all_time_since_today')
+    const req = await fetch('https://wakatime.com/api/v1/users/current/all_time_since_today', options)
 
     return {
         props:{
