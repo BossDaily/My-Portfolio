d6a2a603a979491212c6d7b221952ade667e6201
BossDaily <gboys2008@gmail.com>
2022-08-13 16:57:17 -0500

    added padding for the text

--- components/Title.tsx
+++ components/Title.tsx
@@ -11,7 +11,7 @@ type Props = {
 const Title: NextPage<Props> = ({ children }) => {
     
     return (
-        <div className="font-helvetica text-white text-4xl font-bold">
+        <div className="font-helvetica text-white text-6xl font-bold p-10">
           {children}
         </div>
     );
