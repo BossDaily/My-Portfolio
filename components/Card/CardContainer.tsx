3071f467cac0e22b0ccb863f13483cfe0003f4ca
BossDaily <gboys2008@gmail.com>
2022-08-14 16:43:51 -0500

    css for flex and grid

--- components/Card/CardContainer.tsx
+++ components/Card/CardContainer.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const CardContainer: NextPage<Props> = ({children}) => {
   return (
-    <div>
+    <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8 rounded-xl flex flex-col sm:flex-row">
       {children}
     </div>
   );
