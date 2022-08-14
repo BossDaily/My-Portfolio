437ddb1345e24d7e5f1ae9e6773c1d5e94cb0778
BossDaily <gboys2008@gmail.com>
2022-08-14 17:59:08 -0500

    2nd div for more consistent sizes

--- components/Card/CardContainer.tsx
+++ components/Card/CardContainer.tsx
@@ -1,17 +1,18 @@
 import { NextPage } from "next";
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
 type Props = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
-const CardContainer: NextPage<Props> = ({children}) => {
+const CardContainer: NextPage<Props> = ({ children }) => {
   return (
     <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8 rounded-xl flex flex-col sm:flex-row">
-      {children}
+      <div className="flex flex-col gap-8 rounded-xl lg:flex-row">
+        {children}
+      </div>
     </div>
   );
-}
-
+};
 
 export default CardContainer;
