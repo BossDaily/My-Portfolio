3b3d2712f710ad0bf5206695601f7ed5a1dd7571
BossDaily <gboys2008@gmail.com>
2022-08-14 16:39:22 -0500

    added props to component

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -13,26 +13,23 @@ type Props = {
 const Card: NextPage<Props> = ({ children, title, link, thumbnail, btnName }) => {
   return (
     <div className="bg-Windsor rounded-xl max-w-sm shadow-md">
-      <a href="#">
+      <a href={link}>
         <img
-          src="https://media.discordapp.net/attachments/762750140250718220/1008154919372599366/unknown.png?width=1456&height=910"
+          src={thumbnail}
           className="rounded-t-xl"
         />
       </a>
       <div className="p-5">
         <a className="">
           <h5 className="text-2xl mb-2 font-bold font-helvetica tracking-tight ">
-            ComparatorCraftSMP
+            {title}
           </h5>
         </a>
         <p className="mb-3 ">
-          ComparatorCraft is a 1.19 Hermitcraft like server. We are a server
-          like hermitcraft in the sense that we are a more tight knit community,
-          with a shopping district, and similar features to the iconic
-          hermitcraft server.
+          {children}
         </p>
-        <a href="#">
-          <Button color="purple">Blog</Button>
+        <a href={link}>
+          <Button color="purple">{btnName}</Button>
         </a>
       </div>
     </div>
