d19a79202283b2906506e596476bd34042a14427
BossDaily <gboys2008@gmail.com>
2022-08-14 17:29:24 -0500

    card component for observer stats

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,28 @@
-import type { NextComponentType, NextPage, GetServerSideProps, InferGetServerSidePropsType } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import BottomVectorWaves from '../components/Waves/BottomVectorWaves'
-import TopVectorWaves from '../components/Waves/TopVectorWaves'
-import Avatar from '../components/Avatar/Avatar'
-import HeroContainer from '../components/Hero/HeroContainer'
-import TypewriterComponent from 'typewriter-effect'
-import StatsContainer from '../components/Stats/StatsContainer'
-import StatsItem from '../components/Stats/StatsItem'
-import { gql } from '@apollo/client'
-import client from '../apollo-client'
-import Title from '../components/Title'
-import Card from '../components/Card/Card'
-import CardContainer from '../components/Card/CardContainer'
+import type {
+  NextComponentType,
+  NextPage,
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+} from "next";
+import Head from "next/head";
+import Image from "next/image";
+import BottomVectorWaves from "../components/Waves/BottomVectorWaves";
+import TopVectorWaves from "../components/Waves/TopVectorWaves";
+import Avatar from "../components/Avatar/Avatar";
+import HeroContainer from "../components/Hero/HeroContainer";
+import TypewriterComponent from "typewriter-effect";
+import StatsContainer from "../components/Stats/StatsContainer";
+import StatsItem from "../components/Stats/StatsItem";
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
+import Title from "../components/Title";
+import Card from "../components/Card/Card";
+import CardContainer from "../components/Card/CardContainer";
 
-
-const Home: NextPage = ({codeTime, github}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const Home: NextPage = ({
+  codeTime,
+  github,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <div className="m-0 font-helvetica text-white bg-[#10002B]">
       <section>
@@ -24,68 +31,138 @@ const Home: NextPage = ({codeTime, github}: InferGetServerSidePropsType<typeof g
           <div className="flex-none flex-nowrap">
             <Avatar />
           </div>
-            <TypewriterComponent
-              options={{
-                strings: ['Discord Bot Dev', 'Web Developer', 'Aspiring App Developer', 'Open Source Enthusiast', 'Minecraft server owner', 'Sigma Male'],
-                autoStart: true,
-                loop: true
-                
-              }}
-            />  
+          <TypewriterComponent
+            options={{
+              strings: [
+                "Discord Bot Dev",
+                "Web Developer",
+                "Aspiring App Developer",
+                "Open Source Enthusiast",
+                "Minecraft server owner",
+                "Sigma Male",
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
         </HeroContainer>
       </section>
-      <TopVectorWaves url={'hero_layered_waves1.svg'} />
+      <TopVectorWaves url={"hero_layered_waves1.svg"} />
       <section className="bg-[#C77DFF]">
-        <div className="items-center" >
+        <div className="items-center">
           <StatsContainer>
-            <StatsItem stat={`${github.data.user.repositoriesContributedTo.totalCount} Projects`} icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'/>
-            <StatsItem stat={`${codeTime.data.text} of programming`} icon='https://site-assets.fontawesome.com/releases/v6.1.1/svgs/solid/display-code.svg'/>
-            <StatsItem stat={`${github.data.user.contributionsCollection.totalCommitContributions} Contributions`} icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg'/>
+            <StatsItem
+              stat={`${github.data.user.repositoriesContributedTo.totalCount} Projects`}
+              icon="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+            />
+            <StatsItem
+              stat={`${codeTime.data.text} of programming`}
+              icon="https://site-assets.fontawesome.com/releases/v6.1.1/svgs/solid/display-code.svg"
+            />
+            <StatsItem
+              stat={`${github.data.user.contributionsCollection.totalCommitContributions} Contributions`}
+              icon="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+            />
           </StatsContainer>
         </div>
       </section>
-      <BottomVectorWaves url={'stats_bottom_wave2.svg'} />
-      <section >
+      <BottomVectorWaves url={"stats_bottom_wave2.svg"} />
+      <section>
         <Title>Projects</Title>
         <CardContainer>
-          <Card />
-          <Card />
-          <Card />
+          <Card btnName="Repo" link="https://github.com/ComparatorCraftSMP/observer-bot" title="Observer-Bot" thumbnail="https://media.discordapp.net/attachments/762750022495764491/1008492252978479204/Discord_gjFYUMtrES.png">
+            Observer Bot is a Discord bot I built for my Minecraft server in
+            Discord.js. This bot mainly queries stats from a Minecraft server
+            with ServerTAP installed and shows them in a Discord embed. I made
+            this because a lot of other solutions to do this sucked so I thought
+            I would make one myself.
+          </Card>
+          <Card btnName="Repo" link="https://github.com/ComparatorCraftSMP/observer-bot" title="Observer-Bot" thumbnail="https://media.discordapp.net/attachments/762750022495764491/1008492252978479204/Discord_gjFYUMtrES.png">
+            Observer Bot is a Discord bot I built for my Minecraft server in
+            Discord.js. This bot mainly queries stats from a Minecraft server
+            with ServerTAP installed and shows them in a Discord embed. I made
+            this because a lot of other solutions to do this sucked so I thought
+            I would make one myself.
+          </Card>
+          <Card btnName="Repo" link="https://github.com/ComparatorCraftSMP/observer-bot" title="Observer-Bot" thumbnail="https://media.discordapp.net/attachments/762750022495764491/1008492252978479204/Discord_gjFYUMtrES.png">
+            Observer Bot is a Discord bot I built for my Minecraft server in
+            Discord.js. This bot mainly queries stats from a Minecraft server
+            with ServerTAP installed and shows them in a Discord embed. I made
+            this because a lot of other solutions to do this sucked so I thought
+            I would make one myself.
+          </Card>
+          <Card btnName="Repo" link="https://github.com/ComparatorCraftSMP/observer-bot" title="Observer-Bot" thumbnail="https://media.discordapp.net/attachments/762750022495764491/1008492252978479204/Discord_gjFYUMtrES.png">
+            Observer Bot is a Discord bot I built for my Minecraft server in
+            Discord.js. This bot mainly queries stats from a Minecraft server
+            with ServerTAP installed and shows them in a Discord embed. I made
+            this because a lot of other solutions to do this sucked so I thought
+            I would make one myself.
+          </Card>
         </CardContainer>
       </section>
       <TopVectorWaves url="bottom-wave4.svg" />
       <section className="bg-[#C77DFF]">
         <h1>I am BossDaily</h1>
-        <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
+        <p>
+          I am BossDaily, one of the greatest programmers to ever touch a
+          computer. I program using pen and paper, I remember and then manually
+          switch transistors. Compiliers? Languages? Only losers use those. I
+          can make any program I can think of within 2 hours, I've made fully
+          custom everything because I am afraid the government has a backdoor in
+          everything. I live in a van and I travel america living off of
+          fastfood, willpower, and github sponsorships.
+        </p>
       </section>
       <BottomVectorWaves url="top-layered-waves4.svg" />
       <section>
         <h1>I am BossDaily</h1>
-        <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
+        <p>
+          I am BossDaily, one of the greatest programmers to ever touch a
+          computer. I program using pen and paper, I remember and then manually
+          switch transistors. Compiliers? Languages? Only losers use those. I
+          can make any program I can think of within 2 hours, I've made fully
+          custom everything because I am afraid the government has a backdoor in
+          everything. I live in a van and I travel america living off of
+          fastfood, willpower, and github sponsorships.
+        </p>
       </section>
       <TopVectorWaves url="bottom-layered-waves2.svg" />
       <section className="bg-[#C77DFF]">
         <h1>I am BossDaily</h1>
-        <p>I am BossDaily, one of the greatest programmers to ever touch a computer. I program using pen and paper, I remember and then manually switch transistors. Compiliers? Languages? Only losers use those. I can make any program I can think of within 2 hours, I've made fully custom everything because I am afraid the government has a backdoor in everything. I live in a van and I travel america living off of fastfood, willpower, and github sponsorships.</p>
+        <p>
+          I am BossDaily, one of the greatest programmers to ever touch a
+          computer. I program using pen and paper, I remember and then manually
+          switch transistors. Compiliers? Languages? Only losers use those. I
+          can make any program I can think of within 2 hours, I've made fully
+          custom everything because I am afraid the government has a backdoor in
+          everything. I live in a van and I travel america living off of
+          fastfood, willpower, and github sponsorships.
+        </p>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export const getServerSideProps:GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
+  const waka = await fetch(
+    "https://wakatime.com/api/v1/users/current/all_time_since_today",
+    {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Basic ${process.env.WAKATIME}`,
+      },
+    }
+  );
+  const codeTime = await waka.json();
 
-    
-  const waka = await fetch('https://wakatime.com/api/v1/users/current/all_time_since_today', {
-      method: 'GET',
-      headers: {'content-type': 'application/json', 'Authorization': `Basic ${process.env.WAKATIME}` }
-  })
-  const codeTime = await waka.json()
-  
   const github = await client.query({
     query: gql`
-      query($login: String!, $includeUserRepositories: Boolean) {
+      query ($login: String!, $includeUserRepositories: Boolean) {
         user(login: $login) {
-          repositoriesContributedTo(includeUserRepositories: $includeUserRepositories) {
+          repositoriesContributedTo(
+            includeUserRepositories: $includeUserRepositories
+          ) {
             totalCount
           }
           contributionsCollection {
@@ -95,18 +172,17 @@ export const getServerSideProps:GetServerSideProps = async () => {
       }
     `,
     variables: {
-      "login": "BossDaily",
-      "includeUserRepositories": true
-    }
-  })
-  
+      login: "BossDaily",
+      includeUserRepositories: true,
+    },
+  });
+
   return {
-      props:{
-          codeTime,
-          github
-      }
-  }
-}
+    props: {
+      codeTime,
+      github,
+    },
+  };
+};
 
-
-export default Home
+export default Home;
