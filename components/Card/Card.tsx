92cca905b8b98de7c9b11bc75ef6cc8252468e6c
BossDaily <gboys2008@gmail.com>
2022-08-14 09:59:15 -0500

    added test image

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -8,7 +8,9 @@ type Props = {
 const Card: NextPage<Props> = (props) => {
   return (
     <div className="bg-Windsor rounded-xl max-w-sm shadow-md">
-      Enter
+      <a href="#">
+        <img src="https://media.discordapp.net/attachments/762750140250718220/1008154919372599366/unknown.png?width=1456&height=910" className="rounded-t-xl" />
+      </a>
     </div>
   );
 }
