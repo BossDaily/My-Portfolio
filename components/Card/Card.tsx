05353f913c247e3f7bf7ba870390a87163423a19
BossDaily <gboys2008@gmail.com>
2022-08-14 16:13:25 -0500

    wrapped button in a tag and formatted tsx

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -14,12 +14,20 @@ const Card: NextPage<Props> = (props) => {
       </a>
       <div className="p-5">
         <a className="">
-          <h5 className="text-2xl mb-2 font-bold font-helvetica tracking-tight ">ComparatorCraftSMP</h5>
+          <h5 className="text-2xl mb-2 font-bold font-helvetica tracking-tight ">
+            ComparatorCraftSMP
+          </h5>
         </a>
-        <p className="mb-3 ">ComparatorCraft is a 1.19 Hermitcraft like server. We are a server like hermitcraft in the sense that we are a more tight knit community, with a shopping district, and similar features to the iconic hermitcraft server.</p>
-        <Button color='purple'>
-            blog
-        </Button>
+        <p className="mb-3 ">
+          ComparatorCraft is a 1.19 Hermitcraft like server. We are a server
+          like hermitcraft in the sense that we are a more tight knit community,
+          with a shopping district, and similar features to the iconic
+          hermitcraft server.
+        </p>
+        <Button color="purple">Blog</Button>
+        <a href="#">
+          <Button color="purple">Blog</Button>
+        </a>
       </div>
     </div>
   );
