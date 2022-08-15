4c309d959700f31abe9baf2523763b3d443eb15b
BossDaily <gboys2008@gmail.com>
2022-08-14 20:26:19 -0500

    added temp footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -111,7 +111,8 @@ const Home: NextPage = ({
           </Card>
         </CardContainer>
       </section>
-      <TopVectorWaves url="bottom-wave4.svg" />
+      {/*
+        <TopVectorWaves url="bottom-wave4.svg" />
       <section className="bg-[#C77DFF]">
         <h1>I am BossDaily</h1>
         <p>
@@ -137,17 +138,14 @@ const Home: NextPage = ({
           fastfood, willpower, and github sponsorships.
         </p>
       </section>
+      */
+      }
+      
       <TopVectorWaves url="bottom-layered-waves2.svg" />
       <section className="bg-[#C77DFF]">
         <h1>I am BossDaily</h1>
         <p>
-          I am BossDaily, one of the greatest programmers to ever touch a
-          computer. I program using pen and paper, I remember and then manually
-          switch transistors. Compiliers? Languages? Only losers use those. I
-          can make any program I can think of within 2 hours, I've made fully
-          custom everything because I am afraid the government has a backdoor in
-          everything. I live in a van and I travel america living off of
-          fastfood, willpower, and github sponsorships.
+          Made by BossDaily with Typescript, NextJS, Apollo GraphQL, TailwindCSS, Figma, and ❤️
         </p>
       </section>
     </div>
