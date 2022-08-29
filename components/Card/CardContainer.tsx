02abb2782c223add0bce65997f4860c6365c633d
gboys2008@gmail.com <gboys2008@gmail.com>
2022-08-29 11:57:56 -0400

    stuff

--- components/Card/CardContainer.tsx
+++ components/Card/CardContainer.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const CardContainer: NextPage<Props> = ({ children }) => {
   return (
-    <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8 rounded-xl flex flex-col sm:flex-row flex-initial">
+    <div className=" px-4 py-8 mx-auto sm:px-6 lg:px-8 rounded-xl flex flex-col sm:flex-row flex-initial">
       <div className="flex flex-col gap-8 rounded-xl lg:flex-row">
         {children}
       </div>
