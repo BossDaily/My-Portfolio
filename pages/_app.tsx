7f909509fda1f0abd5cdbe7cb25248e1939641ba
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-01 11:47:59 -0400

    moved theme to file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,33 +3,11 @@ import type { AppProps } from "next/app";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import { Flowbite } from "flowbite-react";
 import "../node_modules/@fortawesome/fontawesome-svg-core/styles.css";
+import { customtheme as theme } from "./theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Flowbite
-      theme={{
-        theme: {
-          tab: {
-            tablist: {
-              styles: {
-                pills:
-                  "flex-wrap text-sm font-helvetica text-white dark:text-gray-400",
-              },
-              tabitem: {
-                styles: {
-                  pills: {
-                    active: {
-                      on: "rounded-lg bg-Medium-Purple font-helvetica text-white",
-                      off: "rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white",
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-      }}
-    >
+    <Flowbite theme={{ theme }}>
       <Component {...pageProps} />
     </Flowbite>
   );
