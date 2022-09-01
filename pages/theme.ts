93898134a4669759b66f36805ffbcfc8448b5c74
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-01 11:48:08 -0400

    moved theme to its own json

---
+++ pages/theme.ts
@@ -0,0 +1,22 @@
+import { CustomFlowbiteTheme } from "flowbite-react/lib/esm/components/Flowbite/FlowbiteTheme";
+
+export const customtheme: CustomFlowbiteTheme = {
+  tab: {
+    tablist: {
+      styles: {
+        pills: "flex-wrap text-sm font-helvetica text-white dark:text-gray-400",
+      },
+      tabitem: {
+        styles: {
+          pills: {
+            active: {
+              base: "",
+              on: "rounded-lg bg-Medium-Purple font-helvetica text-white",
+              off: "rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white",
+            },
+          },
+        },
+      },
+    },
+  },
+};
