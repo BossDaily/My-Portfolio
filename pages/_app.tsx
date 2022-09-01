8a51eabb27b5533f1b572c900ed0d0065d700a47
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-01 11:36:31 -0400

    moved flowbite to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css'
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import { Flowbite } from "flowbite-react";
+import "../node_modules/@fortawesome/fontawesome-svg-core/styles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Flowbite
+      theme={{
+        theme: {
+          tab: {
+            tablist: {
+              styles: {
+                pills:
+                  "flex-wrap text-sm font-helvetica text-white dark:text-gray-400",
+              },
+              tabitem: {
+                styles: {
+                  pills: {
+                    active: {
+                      on: "rounded-lg bg-Medium-Purple font-helvetica text-white",
+                      off: "rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white",
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </Flowbite>
+  );
 }
 
-export default MyApp
+export default MyApp;
