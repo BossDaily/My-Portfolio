3acd79b1e55cee952dc6eea867877a80121fadfa
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-02 11:55:38 -0400

    added boilerplate code stolen from cardcontainer

---
+++ components/LangCard/LangCardContainer.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from "next";
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+const LangCardContainer: NextPage<Props> = ({ children }) => {
+  return (
+    <div className=" px-4 py-8 mx-auto sm:px-6 lg:px-8 rounded-xl flex flex-col sm:flex-row flex-initial">
+      <div className="flex flex-col gap-8 rounded-xl lg:flex-row">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default LangCardContainer;
