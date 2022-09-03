6d98215819e55a06f3a393c98e81fefd21103202
BossDaily <gboys2008@gmail.com>
2022-09-03 13:46:15 -0500

    changed padding

--- components/Card/CardContainer.tsx
+++ components/Card/CardContainer.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const CardContainer: NextPage<Props> = ({ children }) => {
   return (
-    <div className=" px-4 py-8 mx-auto sm:px-6 lg:px-8 rounded-xl flex flex-col sm:flex-row flex-initial flex-wrap">
+    <div className=" p-10 mx-auto sm:px-6 lg:px-8 rounded-xl flex flex-col sm:flex-row flex-initial flex-wrap">
       {children}
     </div>
   );
