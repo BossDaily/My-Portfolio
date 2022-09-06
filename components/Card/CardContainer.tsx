dc9fd04da5a43745289569e6d7072b58590f47e0
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-06 11:36:02 -0400

    overflow visible

--- components/Card/CardContainer.tsx
+++ components/Card/CardContainer.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const CardContainer: NextPage<Props> = ({ children }) => {
   return (
-    <div className="mx-auto snap-x snap-proximity sm:px-6 sm:pb-0 pt-4 pb-2 rounded-xl flex sm:flex-none sm:grid flex-cols sm:grid-cols-8 sm:overflow-x-auto md:grid-cols-3 gap-2 md:pt-12">
+    <div className="snap-x snap-proximity sm:px-6 sm:pb-0 pt-4 pb-2 rounded-xl flex sm:flex-none sm:grid flex-cols sm:grid-cols-8 overflow-x-auto md:grid-cols-3 gap-2 md:pt-12 sm:overflow-x-visible">
       {children}
     </div>
   );
