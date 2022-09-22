f58334b8f19215659bcdbe21b46ec5d87d69b8a5
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-22 11:21:21 -0400

    better padding for bigger screens

--- components/Title.tsx
+++ components/Title.tsx
@@ -11,7 +11,7 @@ type Props = {
 const Title: NextPage<Props> = ({ children }) => {
     
     return (
-        <div className="font-helvetica text-white text-6xl font-bold px-4 pt-20 sm:pt-0">
+        <div className="font-helvetica text-white text-6xl font-bold px-4 pt-20 sm:pt-0 sm:pb-8">
           {children}
         </div>
     );
