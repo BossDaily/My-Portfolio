263540f5b702c33b9b653568ed634760821d8779
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-23 11:20:47 -0400

    everything

--- components/LangCard/LangText.tsx
+++ components/LangCard/LangText.tsx
@@ -1,2 +1,12 @@
 import { ReactNode } from "react";
 import { NextComponentType, NextPage } from "next";
+
+type Props = {
+  children: ReactNode;
+};
+
+const LangText: NextPage<Props> = ({ children }) => {
+  return <div className="font-helvetica font-bold text-3xl pt-4">{children}</div>;
+};
+
+export default LangText;
