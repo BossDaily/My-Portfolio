c169b0666d04c878c967f9ba2d177a366ceb7595
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-23 10:56:58 -0400

    imported lang icon

--- components/LangCard/LangCard.tsx
+++ components/LangCard/LangCard.tsx
@@ -1,6 +1,7 @@
 import { NextComponentType, NextPage } from "next";
 import Image from "next/image";
 import { ReactNode } from "react";
+import LangIcon from "./LangIcon";
 
 type Props = {
   lang: string;
@@ -10,15 +11,8 @@ type Props = {
 const LangCard: NextPage<Props> = ({ lang, icon }) => {
   return (
     <div className="h-20 flex gap-2 flex-row w-60 bg-Purple-Heart rounded-xl content-center">
-      <div className="p-2">
-        <Image
-          src={icon}
-          className="rounded-2xl p-2"
-          height={64}
-          width={64}
-          layout="fixed"
-        />
-      </div>
+      <LangIcon icon={icon} />
+
     </div>
   );
 };
