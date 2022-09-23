e707ee09b98dc65c4b020973b0ce4ccc7e6b2e9a
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-23 11:20:39 -0400

    media query

--- components/LangCard/LangCardContainer.tsx
+++ components/LangCard/LangCardContainer.tsx
@@ -7,8 +7,8 @@ type Props = {
 
 const LangCardContainer: NextPage<Props> = ({ children }) => {
   return (
-    <div className=" px-4 py-8 mx-auto sm:px-6 lg:px-8 rounded-xl flex flex-col sm:flex-row flex-initial">
-      <div className="flex flex-col gap-8 rounded-xl lg:flex-row">
+    <div className=" px-4 py-8 mx-auto sm:px-6 lg:px-8 rounded-xl flex flex-col sm:flex-row flex-initial flex-wrap">
+      <div className="flex flex-col gap-8 rounded-xl lg:flex-row md:">
         {children}
       </div>
     </div>
