562b2857a750d07fc983d6b8be14c1761d673625
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-23 10:53:51 -0400

    proper formatting and wrapped image component in div

--- components/LangCard/LangCard.tsx
+++ components/LangCard/LangCard.tsx
@@ -3,16 +3,24 @@ import Image from "next/image";
 import { ReactNode } from "react";
 
 type Props = {
-  lang: string
-  icon: string
+  lang: string;
+  icon: string;
 };
 
-const LangCard: NextPage<Props> = ({lang, icon}) => {
+const LangCard: NextPage<Props> = ({ lang, icon }) => {
   return (
-    <div className="h-20 flex gap-2 flex-row w-60 bg-Purple-Heart rounded-xl">
-      <Image src={icon} className="rounded-2xl p-2" height={64} width={64}/>
+    <div className="h-20 flex gap-2 flex-row w-60 bg-Purple-Heart rounded-xl content-center">
+      <div className="p-2">
+        <Image
+          src={icon}
+          className="rounded-2xl p-2"
+          height={64}
+          width={64}
+          layout="fixed"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LangCard
+export default LangCard;
