363971509b38138edfe4131b7dab6f59e5fa3834
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-26 11:25:37 -0400

    removed center justification

--- components/LangCard/LangCard.tsx
+++ components/LangCard/LangCard.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 const LangCard: NextPage<Props> = ({ lang, icon }) => {
   return (
-    <div className="h-20 flex gap-2 flex-row w-60 bg-Purple-Heart rounded-xl justify-center hover:-translate-y-1 hover:scale-110">
+    <div className="h-20 flex gap-4 flex-row w-60 bg-Purple-Heart rounded-xl  hover:-translate-y-1 hover:scale-110">
       <LangIcon icon={icon} />
       <LangText>
         {lang}
