22b8c622b95ca09b06e9b8e018218d5df67193b5
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-26 11:09:29 -0400

    displays the language instead of "javascript"

--- components/LangCard/LangCard.tsx
+++ components/LangCard/LangCard.tsx
@@ -14,7 +14,7 @@ const LangCard: NextPage<Props> = ({ lang, icon }) => {
     <div className="h-20 flex gap-2 flex-row w-60 bg-Purple-Heart rounded-xl justify-center hover:-translate-y-1 hover:scale-110">
       <LangIcon icon={icon} />
       <LangText>
-        Javascript
+        {lang}
       </LangText>
     </div>
   );
