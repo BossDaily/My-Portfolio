d8b0b181f01b1bc90af794134ea90285e331700d
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-26 10:53:02 -0400

    more responsive on desktop

--- components/LangCard/LangCardContainer.tsx
+++ components/LangCard/LangCardContainer.tsx
@@ -8,7 +8,7 @@ type Props = {
 const LangCardContainer: NextPage<Props> = ({ children }) => {
   return (
     <div className=" px-4 py-8 mx-auto sm:px-6 lg:px-8 rounded-xl flex flex-col sm:flex-row flex-initial flex-wrap">
-      <div className="flex flex-col gap-8 rounded-xl lg:flex-row md:">
+      <div className="flex flex-col gap-8 rounded-xl lg:grid lg:grid-row lg:grid-cols-3 xl:grid-cols-5">
         {children}
       </div>
     </div>
