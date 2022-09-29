3684ed72bea883f07088187da9202a5baceb39a7
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-29 18:26:17 -0400

    custom status

---
+++ components/Profile/ProfileCustomStatus.tsx
@@ -0,0 +1,9 @@
+import { NextComponentType, NextPage } from "next";
+import Image from "next/image";
+import { ReactNode } from "react";
+
+const ProfileCustomStatus: NextPage = () => {
+  return <div></div>
+};
+
+export default ProfileCustomStatus;
