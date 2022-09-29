4679bbc46ef6d11092514ee14a910432b29f4147
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-29 11:45:50 -0400

    Profile Activity Boileplate

---
+++ components/Profile/ProfileActivityContainer.tsx
@@ -0,0 +1,9 @@
+import { NextComponentType, NextPage } from "next";
+import Image from "next/image";
+import { ReactNode } from "react";
+
+const ProfileActivityContainer: NextPage = () => {
+  return <div></div>
+};
+
+export default ProfileActivityContainer;
