d094cf5b7804f691d7e6cc25dff05048b472fd94
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-30 11:29:07 -0400

    query

--- components/Profile/ProfileCard.tsx
+++ components/Profile/ProfileCard.tsx
@@ -1,18 +1,16 @@
-import { NextComponentType, NextPage } from "next";
+import { GetServerSideProps, NextComponentType, NextPage } from "next";
 import Image from "next/image";
 import { ReactNode } from "react";
 import { useLanyard } from "react-use-lanyard/dist";
 
 import type { Activity } from "react-use-lanyard/dist";
-import discordProfile from "./discordProfile";
-
 
 const ProfileCard: NextPage = () => {
   const { loading, status } = useLanyard({
     userId: "274973338676494347",
     socket: true,
   });
-  
+
   return (
     <div className="w-80 h-full bg-Medium-Purple flex flex-col rounded-lg">
       <h1>fortnite</h1>
@@ -20,4 +18,23 @@ const ProfileCard: NextPage = () => {
   );
 };
 
+export const getServerSideProps: GetServerSideProps = async () => {
+  const discordQuery = await fetch(
+    "https://discord.com/api/users/274973338676494347",
+    {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${process.env.DISCORD_SECRET}`,
+      },
+    }
+  );
+  const discord = await discordQuery.json();
+
+  return {
+    props: {
+      discord,
+    },
+  };
+};
 export default ProfileCard;
