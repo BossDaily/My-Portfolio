56b300c2e8f864d7517db7d849e63bac118902e4
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-30 11:08:08 -0400

    css and fetches

--- components/Profile/ProfileCard.tsx
+++ components/Profile/ProfileCard.tsx
@@ -2,14 +2,20 @@ import { NextComponentType, NextPage } from "next";
 import Image from "next/image";
 import { ReactNode } from "react";
 import { useLanyard } from "react-use-lanyard/dist";
+
 import type { Activity } from "react-use-lanyard/dist";
 
 const ProfileCard: NextPage = () => {
-  const { loading, status } = useLanyard({
+  const { loading, status  } = useLanyard({
     userId: "274973338676494347",
     socket: true,
   })
-  return <div></div>
+  const discord = await fetch('https://discord.com/api/users/274973338676494347')
+  return (
+    <div className="w-80 h-full bg-Medium-Purple flex flex-col rounded-lg">
+      <h1>fortnite</h1>
+    </div>
+  )
 };
 
 export default ProfileCard;
