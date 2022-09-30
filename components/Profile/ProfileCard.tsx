7fd5aa4f659bb6c9b8decd6f832b7edcfff2b802
gboys2008@gmail.com <gboys2008@gmail.com>
2022-09-30 11:23:42 -0400

    removed attempts of me querying discord api

--- components/Profile/ProfileCard.tsx
+++ components/Profile/ProfileCard.tsx
@@ -4,18 +4,20 @@ import { ReactNode } from "react";
 import { useLanyard } from "react-use-lanyard/dist";
 
 import type { Activity } from "react-use-lanyard/dist";
+import discordProfile from "./discordProfile";
+
 
 const ProfileCard: NextPage = () => {
-  const { loading, status  } = useLanyard({
+  const { loading, status } = useLanyard({
     userId: "274973338676494347",
     socket: true,
-  })
-  const discord = await fetch('https://discord.com/api/users/274973338676494347')
+  });
+  
   return (
     <div className="w-80 h-full bg-Medium-Purple flex flex-col rounded-lg">
       <h1>fortnite</h1>
     </div>
-  )
+  );
 };
 
 export default ProfileCard;
