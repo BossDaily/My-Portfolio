3fab59c4ab84da795165e3b18f645fb84b9939c5
BossDaily <gboys2008@gmail.com>
2022-10-01 13:24:06 -0500

    switch statement for status

--- components/Profile/ProfilePic.tsx
+++ components/Profile/ProfilePic.tsx
@@ -7,14 +7,39 @@ interface Props {
   status: string;
 }
 const ProfilePic: NextPage<Props> = (props) => {
-  const { url, status} = props
-  const loaderProp =({ src, width }: ImageLoaderProps) => {
+  const { url, status } = props;
+  const loaderProp = ({ src, width }: ImageLoaderProps) => {
     return `${src}?size=${width}`;
+  };
+  switch (status) {
+    case "online":
+      return (
+        <div className="relative p-2">
+          <Image
+            src={url}
+            height={128}
+            width={128}
+            loader={loaderProp}
+            className="rounded-full"
+          />
+          <span className="bottom-2 left-24 absolute  w-9 h-9 bg-green-400 border-2 border-Medium-Purple rounded-full"></span>
+        </div>
+      );
+      break;
+
+    default:
+      break;
   }
   return (
     <div className="relative p-2">
-      <Image src={url} height={128} width={128} loader={loaderProp} className="rounded-full"/>
-      <span className="bottom-2 left-24 absolute  w-10 h-10 bg-green-400 border-2 border-Medium-Purple rounded-full"></span>
+      <Image
+        src={url}
+        height={128}
+        width={128}
+        loader={loaderProp}
+        className="rounded-full"
+      />
+      <span className="bottom-2 left-24 absolute  w-9 h-9 bg-green-400 border-2 border-Medium-Purple rounded-full"></span>
     </div>
   );
 };
