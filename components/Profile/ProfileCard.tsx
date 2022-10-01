0d68c6bfc2eb0762607e3146c4182d9abe284434
BossDaily <gboys2008@gmail.com>
2022-10-01 10:02:07 -0500

    imported profilepic

--- components/Profile/ProfileCard.tsx
+++ components/Profile/ProfileCard.tsx
@@ -11,6 +11,7 @@ import { ReactNode } from "react";
 import { useLanyard } from "react-use-lanyard/dist";
 
 import type { Activity } from "react-use-lanyard/dist";
+import ProfilePic from "./ProfilePic";
 
 const ProfileCard: NextPage = () => {
   const { loading, status } = useLanyard({
@@ -21,6 +22,7 @@ const ProfileCard: NextPage = () => {
   return (
     <div className="w-80 h-full bg-Medium-Purple flex flex-col rounded-lg">
       <h1>fortnite</h1>
+      <ProfilePic />
     </div>
   );
 };
