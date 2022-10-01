b7c3184a7abbb84ef37067d0932feda1a82850db
BossDaily <gboys2008@gmail.com>
2022-10-01 13:33:07 -0500

    added case for dnd and changed default to offline

--- components/Profile/ProfilePic.tsx
+++ components/Profile/ProfilePic.tsx
@@ -39,19 +39,36 @@ const ProfilePic: NextPage<Props> = (props) => {
           <span className="bottom-2 left-24 absolute  w-9 h-9 bg-yellow-400 border-2 border-Medium-Purple rounded-full"></span>
         </div>
       );
-    
-    default:
-      <div className="relative p-2">
-        <Image
-          src={url}
-          height={128}
-          width={128}
-          loader={loaderProp}
-          className="rounded-full"
-        />
-        <span className="bottom-2 left-24 absolute  w-9 h-9 bg-gray-300 border-2 border-Medium-Purple rounded-full"></span>
-      </div>;
       break;
+    case "dnd":
+      return (
+        <div className="relative p-2">
+          <Image
+            src={url}
+            height={128}
+            width={128}
+            loader={loaderProp}
+            className="rounded-full"
+          />
+          <span className="bottom-2 left-24 absolute  w-9 h-9 bg-red-400 border-2 border-Medium-Purple rounded-full"></span>
+        </div>
+      );
+      break;
+    case "offline": 
+    return(
+      <div className="relative p-2">
+      <Image
+        src={url}
+        height={128}
+        width={128}
+        loader={loaderProp}
+        className="rounded-full"
+      />
+      <span className="bottom-2 left-24 absolute  w-9 h-9 bg-gray-500 border-2 border-Medium-Purple rounded-full"></span>
+    </div>
+    )
+    break;
+    
   }
   return (
     <div className="relative p-2">
