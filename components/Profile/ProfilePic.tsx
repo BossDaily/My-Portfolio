53ca49264628f4c95686ffe4f0da28ac3595e6de
BossDaily <gboys2008@gmail.com>
2022-10-01 13:11:35 -0500

    test span

--- components/Profile/ProfilePic.tsx
+++ components/Profile/ProfilePic.tsx
@@ -14,6 +14,7 @@ const ProfilePic: NextPage<Props> = (props) => {
   return (
     <div className="relative">
       <Image src={url} height={128} width={128} loader={loaderProp} className="rounded-full"/>
+      <span className="bottom-1 left-20 absolute  w-8 h-8 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
     </div>
   );
 };
