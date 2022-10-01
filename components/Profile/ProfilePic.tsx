dea89a485a57f2338fd597cdbd48380df9c97e1f
BossDaily <gboys2008@gmail.com>
2022-10-01 10:06:18 -0500

    height and width

--- components/Profile/ProfilePic.tsx
+++ components/Profile/ProfilePic.tsx
@@ -2,18 +2,16 @@ import { NextComponentType, NextPage } from "next";
 import Image from "next/image";
 import { ReactNode } from "react";
 
-
 interface Props {
-  url: string
-  status: string
+  url: string;
+  status: string;
 }
-const ProfilePic: NextPage<Props> = ({url, status}) => {
-  
+const ProfilePic: NextPage<Props> = ({ url, status }) => {
   return (
     <div className="relative">
-      <Image src={url} />
+      <Image src={url} height={200} width={200} />
     </div>
-  )
+  );
 };
 
 export default ProfilePic;
