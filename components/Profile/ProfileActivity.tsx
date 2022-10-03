f6787309e53eaff53f6204afa2a6dbbe8feca14c
gboys2008@gmail.com <gboys2008@gmail.com>
2022-10-03 11:15:32 -0400

    added props

--- components/Profile/ProfileActivity.tsx
+++ components/Profile/ProfileActivity.tsx
@@ -2,8 +2,22 @@ import { NextComponentType, NextPage } from "next";
 import Image from "next/image";
 import { ReactNode } from "react";
 
-const ProfileActivity: NextPage = () => {
-  return <div></div>
+interface Props {
+  largeImage: string
+  smallImage: string
+  name: string
+  details: string
+  state: string
+}
+
+const ProfileActivity: NextPage<Props> = (props) => {
+  const {largeImage, smallImage, name, details, state} = props
+
+  return ( 
+    <div>
+      
+    </div>
+  )
 };
 
 export default ProfileActivity;
