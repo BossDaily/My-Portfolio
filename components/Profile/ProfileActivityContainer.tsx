16849cca4fea0223af1726a82e7edcd7c73e8c93
gboys2008@gmail.com <gboys2008@gmail.com>
2022-10-03 11:01:17 -0400

    imported lanyard

--- components/Profile/ProfileActivityContainer.tsx
+++ components/Profile/ProfileActivityContainer.tsx
@@ -1,8 +1,15 @@
 import { NextComponentType, NextPage } from "next";
 import Image from "next/image";
 import { ReactNode } from "react";
+import { useLanyard } from "react-use-lanyard/dist";
+import type { Activity } from "react-use-lanyard/dist";
 
 const ProfileActivityContainer: NextPage = () => {
+  const { loading, status } = useLanyard({
+    userId: "274973338676494347",
+    socket: true,
+  });
+  
   return <div></div>
 };
 
