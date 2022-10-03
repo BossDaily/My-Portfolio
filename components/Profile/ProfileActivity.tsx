ddb7501c814c86e2b3991ba5e1ace2d5d4541dea
gboys2008@gmail.com <gboys2008@gmail.com>
2022-10-03 11:45:34 -0400

    better rounded corners

--- components/Profile/ProfileActivity.tsx
+++ components/Profile/ProfileActivity.tsx
@@ -3,37 +3,36 @@ import Image, { ImageLoaderProps } from "next/image";
 import { ReactNode } from "react";
 
 interface Props {
-  largeImage: string
-  smallImage: string
-  name: string
-  details: string
-  state: string
-  applicationId: string
+  largeImage: string;
+  smallImage: string;
+  name: string;
+  details: string;
+  state: string;
+  applicationId: string;
 }
 
 const ProfileActivity: NextPage<Props> = (props) => {
-  const {largeImage, smallImage, name, details, state, applicationId} = props
+  const { largeImage, smallImage, name, details, state, applicationId } = props;
 
   const loaderProp = ({ src, width }: ImageLoaderProps) => {
     return `${src}?size=${width}`;
   };
 
-  return ( 
+  return (
     <div className="flex flex-row gap-1 p-2">
       <div className="relative">
-      <Image
-            src={url}
-            height={128}
-            width={128}
-            loader={loaderProp}
-            className="rounded-full"
-          />
+        <Image
+          src={`https://cdn.discordapp.com/app-assets/${applicationId}/${largeImage}.png`}
+          height={64}
+          width={64}
+          loader={loaderProp}
+          className="rounded-xl"
+        />
+        
       </div>
-      <div>
-
-      </div>
+      <div></div>
     </div>
-  )
+  );
 };
 
 export default ProfileActivity;
