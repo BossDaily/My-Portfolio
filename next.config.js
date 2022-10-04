cc9f14df98a251c938b3cceff771240e42cda0d8
gboys2008@gmail.com <gboys2008@gmail.com>
2022-10-04 11:43:22 -0400

    added spotify cdn

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['cdn.discordapp.com', 'cdn.jsdelivr.net', 'site-assets.fontawesome.com', 'seeklogo.com', 'resources.jetbrains.com', 'media.discordapp.net'],
+    domains: ['cdn.discordapp.com', 'cdn.jsdelivr.net', 'site-assets.fontawesome.com', 'seeklogo.com', 'resources.jetbrains.com', 'media.discordapp.net', 'i.scdn.co'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
