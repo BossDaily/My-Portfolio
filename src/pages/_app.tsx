44ffef345bee611bc782ef680c51c7d1ba14aceb
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-05-31 18:16:26 +0000

    Fixed import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import { Flowbite } from "flowbite-react";
-import "../node_modules/@fortawesome/fontawesome-svg-core/styles.css";
+import "../../node_modules/@fortawesome/fontawesome-svg-core/styles.css";
 import { customtheme as theme } from "../../../components/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
