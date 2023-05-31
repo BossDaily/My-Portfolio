1529ee5d77618861bd09ed79920e3212ef0ea7d7
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-05-31 18:25:58 +0000

    added back old tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        waves:
+          "https://raw.githubusercontent.com/BossDaily/My-Portfolio/main/public/stats_bottom_wave1.svg",
       },
+      colors: {
+        BlackRussian: "#10002B",
+        Windsor: "#3C096C",
+        Heliotrope: "#C77DFF",
+        "Medium-Purple": "#B469EF",
+        "Purple-Heart": "#7B2CBF",
+      },
+    },
+    fontFamily: {
+      header: ["Helvetica", "Arial", "sans-serif"],
     },
   },
   plugins: [],
-}
+};
