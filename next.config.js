93c8c0e5a0378204dc6c4f6fb31062ccce1bbd61
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-05-31 18:16:45 +0000

    uses appdir

--- next.config.js
+++ next.config.js
@@ -13,6 +13,9 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
+  experimental: {
+    appDir: true,
+  }
 };
 
 module.exports = nextConfig;
