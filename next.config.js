b7e8c6f40debaa7cd12bed843812e6df95b20101
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-11-01 17:26:50 +0000

    updated image config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,20 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['cdn.discordapp.com', 'cdn.jsdelivr.net', 'site-assets.fontawesome.com', 'seeklogo.com', 'resources.jetbrains.com', 'media.discordapp.net', 'i.scdn.co'],
+    remotePatterns: [
+      { hostname: "**.discordapp.net" },
+      { hostname: "**.discord.com" },
+      { hostname: "**.discordapp.com" },
+      { hostname: "**.jetbrains.com" },
+      { hostname: "**.jsdelivr.net" },
+      { hostname: "seeklogo.com" },
+      { hostname: "**.fontawesome.com" },
+      { hostname: "**.scdn.co" },
+    ],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   typescript: {
-    ignoreBuildErrors: true
-  }
-}
+    ignoreBuildErrors: true,
+  },
+};
